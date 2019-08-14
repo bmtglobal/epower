9ae936b1c4b2b2fc52e3f1a0830e23295c56f0ae
beckyfisher <rebeccafisher76@gmail.com>
2019-08-14 08:45:26 +0800

    Added startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,15 @@
 
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to my package")
+  packageStartupMessage("‘epower’ is freely available to users under
+                        licence which precludes the incorporation of the ‘epower’
+                        code into other programs which are subsequently sold or used for
+                        commercial purposes. Please include the following acknowledgment when
+                        using ‘epower’:  “Analyses were carried out using the software ‘epower’
+                        V1.3 (BMT 2019) as described in Fisher et al (2019), (BMT 2019) and
+                        based on the statistical programming platform R (R-Core Team, 2019).
+                        ‘epower’ has been prepared jointly by BMT, the Australian Institute of
+                        Marine Science and Queensland University of Technology. BMT, the
+                        Australian Institute of Marine Science and Queensland University of
+                        Technology accept no liability or responsibility for in respect of
+                        any use of or reliance upon this software.")
 }
