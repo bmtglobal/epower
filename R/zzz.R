3d4df751eff308ac074bb2b1cf6336c7328c84aa
beckyfisher <rebeccafisher76@gmail.com>
2019-08-14 10:19:21 +0800

    Minor edits to welcome message

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   packageStartupMessage("‘epower’ is freely available to users under licence which \n",
 "precludes the incorporation of the ‘epower’ code into other programs which are \n",
 "subsequently sold or used for commercial purposes. Please include the following \n",
-"acknowledgment when using ‘epower’:  “Analyses were carried out using the software \n",
+"acknowledgment when using ‘epower’:  Analyses were carried out using the software \n",
 "‘epower’ V1.3 (BMT 2019) as described in Fisher et al (2019) and (BMT 2019) and based \n",
 "on the statistical programming platform R (R-Core Team, 2019). ‘epower’ has been \n",
 "developed jointly by BMT, the Australian Institute of marine Science and Queensland \n",
